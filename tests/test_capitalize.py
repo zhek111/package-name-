5f2db8271dc3b8f9e57aaee8743d48b0c9014339
evgeny <zhek12322@gmail.com>
2022-07-14 12:21:47 +0300

    step1

---
+++ tests/test_capitalize.py
@@ -0,0 +1,9 @@
+from srs.capitalize import capitalize
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
